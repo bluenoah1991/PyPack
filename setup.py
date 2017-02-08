5b11d55117b1b35c8a2cce0a8505ae9c32000796
CodeMeow <codemeow@yahoo.com>
2017-02-08 15:47:35 +0800

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'pypack2',
-	version = '0.1.1',
+	version = '0.1.2',
 	author = 'CodeMeow5',
 	author_email = 'codemeow@yahoo.com',
 	description = 'Python HttpPack server-side plugin based on gevent',
